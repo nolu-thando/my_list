ecd0d679a6c13ee7399ee71f98f91e8fdb3143e0
Noluthando Mphande <noluthandowendy2013@gmail.com>
2025-03-14 10:59:05 +0200

    My list was extended

--- my_list.py
+++ my_list.py
@@ -5,6 +5,9 @@ print("Before Append" ,my_list)
 my_list.append(20)
 
 print("After Append",my_list)
-
+#added my_list2 so that I can use the append method.
 my_list2 = [50,60,70,80]
 print(my_list2)
+
+my_list.extend(my_list2)
+print("List after append",my_list)
