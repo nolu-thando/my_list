84138bfd46e230074e7b7c76eeff036777975494
Noluthando Mphande <noluthandowendy2013@gmail.com>
2025-03-25 11:09:55 +0200

    added the remove function

--- my_list.py
+++ my_list.py
@@ -13,5 +13,10 @@ my_list.extend(my_list2)
 print("List after append",my_list)
 
 #removing the last element from my_list2
-my_list2.remove(80)
-print(my_list2)
+my_list =[10,20,30,40]
+removedValue = my_list.remove (40)
+print('My list after removing the last number:', my_list)
+
+##sorting my list in ascending order.
+my_list =[10,20,30,40]
+my_list.sort
